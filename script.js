b6f2e82fd62861094ecccc71b3c41ee4503edc75
ArnildoSoares <arnildo-soares@hotmail.com>
2023-01-13 21:56:07 -0300

    update commit

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function markAllAsRead(){
     document.getElementById('counter').innerHTML= '0';
     if(document.getElementsByClassName('msg-unread').length){
        do {
-        var teste = document.getElementsByClassName('msg-unread');
-        teste[0].classList.remove('msg-unread');
+        var msgUnRead = document.getElementsByClassName('msg-unread');
+        msgUnRead[0].classList.remove('msg-unread');
         } while (document.getElementsByClassName('msg-unread').length); 
     }
     
